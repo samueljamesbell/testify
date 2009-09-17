1836e87b9a8c138983636b947b78768b9b8bed18
Sam Bell <sam@samsfedora.(none)>
2009-09-17 22:08:47 +0100

    Commented user.rb#11

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   	handle
   end
 
-	has_attached_file :logo, :styles => { :thumb => '100x100', :small => '250x250>'}
+#	has_attached_file :logo, :styles => { :thumb => '100x100', :small => '250x250>'}
 	
 	validates_presence_of     :email
 	validates_presence_of     :name
