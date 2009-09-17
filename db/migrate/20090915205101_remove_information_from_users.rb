1fc27bbd83bbcd4aca52d93adf73e675d174431d
Sam Bell <sam@samsfedora.(none)>
2009-09-17 21:43:08 +0100

    Trial

--- db/migrate/20090915205101_remove_information_from_users.rb
+++ db/migrate/20090915205101_remove_information_from_users.rb
@@ -1,11 +1,8 @@
 class RemoveInformationFromUsers < ActiveRecord::Migration
   def self.up
   	remove_column :users, :organisation
-  	remove_column :users, :company
   end
 
   def self.down
-    add_column :users, :organisation
-  	add_column :users, :company
   end
 end
