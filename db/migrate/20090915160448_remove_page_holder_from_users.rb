1f04909451f2314391ac402127f9efbf664f04a5
Sam Bell <sam@samsfedora.(none)>
2009-09-17 21:34:12 +0100

    Cleaned up migrations

--- db/migrate/20090915160448_remove_page_holder_from_users.rb
+++
@@ -1,9 +0,0 @@
-class RemovePageHolderFromUsers < ActiveRecord::Migration
-  def self.up
-    remove_column :users, :page_holder
-  end
-
-  def self.down
-    remove_column :users, :page_holder
-  end
-end
--- db/migrate/20090915160612_add_company_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddCompanyToUsers < ActiveRecord::Migration
-  def self.up
-    add_column :users, :company, :boolean
-  end
-
-  def self.down
-    remove_column :users, :company
-  end
-end
