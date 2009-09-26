7c50afc8b3e4d4117109abcc9cc272889a1a095e
Sam Bell <sam@samsfedora.(none)>
2009-09-26 23:53:01 +0100

    Updated deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -44,3 +44,11 @@ task :configure_database, :roles => :app do
   db_config = "#{deploy_to}/config/database.yml"
   run "cp #{db_config} #{release_path}/config/database.yml"
 end
+
+after 'deploy:symlink', 'deploy:create_logos_symlinks'
+  desc "Symlinks logos folder"
+  task :create_logos_symlinks, :roles => :app do
+    run "rm -rf #{current_path}/public/logos"
+    run "ln -s #{shared_path}/logos #{current_path}/public/logos"
+  end
+
